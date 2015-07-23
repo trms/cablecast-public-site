20ea6dca3107d2b4741cf271b60cfff7c6e6af39
raytiley <raytiley@gmail.com>
2015-07-23 16:16:37 -0400

    Transition to first channel if channel not defined

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    channel: {
+      refreshModel: true
+    }
+  },
+
   model: function(params){
+    var self = this;
     return this.store.find('channel')
     .then(function(channels) {
       var channel = channels.findBy('id', params.channel + '');
       if (!channel) {
-        return channels.get('firstObject');
+        self.transitionTo({queryParams: {channel: channels.get('firstObject.id')}})
       } else {
         return channel;
       }
