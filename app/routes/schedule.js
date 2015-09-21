69fff4f38d3661a43738b2d05cb22863530812aa
raytiley <raytiley@gmail.com>
2015-09-21 08:22:50 -0400

    Fix issue calculating schedule time ranges
    We need to sideload teh shows and reels inorder to make the channel models schedule property accurate. If those models aren't loaded with the schedule item the end calculating erros, causing all events to be left in the collection. Ideallly we would refactor the way this works as it doesn't take advantage of data binding.

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -10,12 +10,13 @@ export default Ember.Route.extend({
 	model: function(params){
 		var appParams = this.paramsFor('application');
   		var _start = moment(params.currentDay).startOf('day').format();
-		var _end = moment(params.currentDay).add(1, 'days').format();
+			var _end = moment(params.currentDay).add(1, 'days').format();
 
     	return this.store.find('schedule-item', {
 	    	channel: appParams.channel,
     		start: _start,
-    		end: _end
+    		end: _end,
+				include: 'show,reel'
 	    }).
 			then(function(runs) {
 				return runs.filter(function(run) {
