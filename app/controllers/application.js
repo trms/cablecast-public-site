12d4a512d50edf246221c70841052ecb5985a786
raytiley <raytiley@gmail.com>
2015-09-25 11:37:13 -0400

    Hide Podcast Link if there are no podcasts
    This commit hides the podcasts link in the footer if there are no
    podcasts available on the system.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['channel'],
 
+  projects: Ember.computed('model.primaryLocation.id', function() {
+    return this.store.query('project', {location: this.get('model.primaryLocation.id')})
+  }),
+
+  hasPodcasts: Ember.computed('projects.[]', function() {
+    return this.get('projects').filter(function(project) {
+      // Test that a project has a name and is marked for podcasting.
+      return project.get('name') &&
+             project.get('podcast');
+    }).length > 0;
+  }),
+
   actions: {
 		navSearch: function(query) {
 			this.transitionToRoute('search', {queryParams: {query: query}});
