9d16e94aed9b7b2f1721f5f38f3114c25893e90d
Christian Bechette <christian.bechette@trms.com>
2017-03-02 15:59:51 -0500

    cc-69 Improve sharing of public site URLS on social media
    This adds the appropriate open graph and twitter card tags to the head document so when shared on social media thumbnails, titles, and descriptions are displayed. It also adds social sharing buttons to the show record for Twitter and Facebook.

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import ENV from 'public/config/environment';
 
 export default Ember.Route.extend({
+  fastboot: Ember.inject.service(),
   headData: Ember.inject.service(),
 
   queryParams: {
@@ -10,12 +11,38 @@ export default Ember.Route.extend({
     }
   },
 
-  model: function(params) {
+  setHeadData(channel) {
+    let fastboot = this.get('fastboot');
     let headData = this.get('headData');
+    let url = 'foo';
+    headData.set('channelID', channel.get('id'));
+    headData.set('rootURL', ENV.rootURL);
 
+    if (fastboot.get('isFastBoot')) {
+      let protocol = fastboot.get('request.protocol');
+      let host = fastboot.get('request.host');
+      let path = fastboot.get('request.path');
+      url = `${protocol}://${host}${path}`;
+    } else {
+      url = document.location.href;
+    }
+
+    let publicSite = channel.get('publicSite');
+    let data = {
+      type: 'website',
+      card: 'summary',
+      title: publicSite.get('siteName'),
+      description: publicSite.get('aboutPageDescription'),
+      image: publicSite.get('logo.url'),
+    };
+    headData.set('url', url);
+    headData.set('socialMedia', data);
+  },
+
+  model: function(params) {
     // TODO enable sideloading of publicsite
     return Ember.RSVP.hash({
-      channels: this.get('store').query('channel', {include: 'publicsite'}),
+      channels: this.get('store').query('channel', {include: 'publicsite,webfile'}),
       publicSites: this.get('store').findAll('public-site')
     })
     .then((result) => {
@@ -23,11 +50,15 @@ export default Ember.Route.extend({
       if (!channel) {
         channel = result.channels.get('firstObject');
       }
-      return channel;
+      let logoId = channel.get('publicSite.logo.id');
+      return Ember.RSVP.hash({
+        channel:channel,
+        logo:this.get('store').findRecord('web-file', logoId)
+      });
     })
-    .then((channel) => {
-      headData.set('channelID', channel.get('id'));
-      headData.set('rootURL', ENV.rootURL);
+    .then((results) => {
+      let channel = results.channel;
+      this.setHeadData(channel);
 
       return Ember.RSVP.hash({
         logo: channel.get('publicSite.logo'),
