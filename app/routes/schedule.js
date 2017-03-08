12e119567fc809691d6121d2a469866926c4a9f8
Christian Bechette <christian.bechette@trms.com>
2017-03-08 14:55:20 -0500

    cc-82 update to take advantage of channel side loading caps
    I also removed unused relationships from the channel model.
    
    And the schedule item already had the capability to side load what it
    needed so I cleaned up that route's model.

--- app/routes/schedule.js
+++ app/routes/schedule.js
@@ -12,29 +12,15 @@ export default Ember.Route.extend({
   		var _start = moment(params.currentDay).startOf('day').format();
 			var _end = moment(params.currentDay).add(1, 'days').format();
 
-//TODO: side load vods
     	return this.store.query('schedule-item', {
 	    	channel: appParams.channel,
     		start: _start,
     		end: _end,
-				include: 'show,reel',
+				include: 'show,reel,vod,vodTransaction',
         page_size: -1,
         include_cg_exempt: false
 	    })
 			.then(runs => {
-				return Ember.RSVP.hash({
-					runs:runs,
-					vods:this.get('store').findAll('vod')
-				});
-			})
-			.then(results=>{
-				return Ember.RSVP.hash({
-					runs:results.runs,
-					transactions:this.store.findAll('vod-transaction')
-				});
-			})
-			.then(results => {
-				let runs = results.runs;
 				return runs.filter(function(run) {
 					return run.get('idType') === 1 &&
 								 run.get('cgExempt') === false;
