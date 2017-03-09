56e5c3f41dd7d26a689846d1f817532a81a73579
Christian Bechette <christian.bechette@trms.com>
2017-03-09 14:39:18 -0500

    cc-95 added google schema tags

--- app/components/show-thumbnail/component.js
+++ app/components/show-thumbnail/component.js
@@ -3,15 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'img',
 	quality: 'Small',
-	attributeBindings: ['src'],
+	attributeBindings: ['src', 'itemprop'],
 	src: Ember.computed.alias('thumbnailPath'),
+	itemprop: 'thumbnailUrl',
 
 	show: null,
 
 	thumbnailPath: Ember.computed('show.showThumbnails.@each.quality', 'quality', {
 		get: function() {
 			var thumbnail = this.get('show.showThumbnails').findBy('quality', this.get('quality'));
-			
+
 			// If we can't find the specifiec quality default to first thumbnail
 			if (!thumbnail) {
 			  thumbnail = this.get('show.showThumbnails.firstObject');
