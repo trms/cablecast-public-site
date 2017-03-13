4b8bd5310a9ae55c97589f727eabf03a4121fc46
Seth Phillips <seth.phillips@trms.com>
2017-03-13 17:07:40 -0500

    cc-105 add channels page with links to change public site channel

--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function() {
   this.route('podcasts');
   this.route('schedule');
   this.route('show', {path: 'show/:id'});
-  this.route('channel');
+  this.route('channels');
   this.route('watch-now', { path:'watch/:stream_id' });
   this.route('vods');
   this.route('search');
