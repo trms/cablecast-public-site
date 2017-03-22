edb535a8dffb375bafc0d22c4bf2005547854fc8
Seth Phillips <seth.phillips@trms.com>
2017-03-22 11:25:11 -0500

    cc-128 refactor the preview schedule display

--- app/routes/watch-now.js
+++ app/routes/watch-now.js
@@ -1,23 +1,21 @@
 import Ember from 'ember';
 import SetPageTitle from 'public/mixins/set-page-title';
+import GetFutureRuns from 'public/mixins/channel-future-runs-promise';
 
-export default Ember.Route.extend(SetPageTitle, {
+export default Ember.Route.extend(SetPageTitle, GetFutureRuns,{
   headData: Ember.inject.service(),
 
   afterModel(model) {
     this.setTitle(model.liveStream.get('name'));
   },
 
-  model: function(params) {
-    return this.store.findRecord('live-stream', params.stream_id).
-  						then(function(liveStream){
-  							var channel = liveStream.get('channel');
-  							return Ember.RSVP.hash({
-  								liveStream: liveStream,
-  								channel: channel,
-  								schedule: channel.get('schedule')
-  							});
-  						});
+  model(params) {
+    let {channel} = this.modelFor('application');
+
+    return Ember.RSVP.hash({
+      futureRuns:this.getFutureRuns(channel),
+      liveStream:this.store.findRecord('live-stream', params.stream_id),
+    });
   }
 
 });
