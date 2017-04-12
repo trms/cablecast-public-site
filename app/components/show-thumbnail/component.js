b19534b8586d7aa08f974271674accfb5ec44ab6
Christian Bechette <christian.bechette@trms.com>
2017-04-12 16:38:52 -0400

    Cc 95 structured data in public site (#58)
    * Revert "cc-95 added google schema tags"
    
    This reverts commit 56e5c3f41dd7d26a689846d1f817532a81a73579.
    
    * CC-95 use json-LD for basic structured data
    
    Replaced the microdata tags by json-LD. The idea is to get basic support
    for structured data using schema.org's vocabulary, as a first step.
    
    * fix for thumbnail url, fix for date published
    
    tested on google's structured data tester, looks good

--- app/components/show-thumbnail/component.js
+++ app/components/show-thumbnail/component.js
@@ -3,16 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'img',
 	quality: 'Small',
-	attributeBindings: ['src', 'itemprop'],
+	attributeBindings: ['src'],
 	src: Ember.computed.alias('thumbnailPath'),
-	itemprop: 'thumbnailUrl',
 
 	show: null,
 
 	thumbnailPath: Ember.computed('show.showThumbnails.@each.quality', 'quality', {
 		get: function() {
 			var thumbnail = this.get('show.showThumbnails').findBy('quality', this.get('quality'));
-
+			
 			// If we can't find the specifiec quality default to first thumbnail
 			if (!thumbnail) {
 			  thumbnail = this.get('show.showThumbnails.firstObject');
