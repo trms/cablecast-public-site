b65a746bcbefb8fa362bd253418b3be3c4e678d9
raytiley <raytiley@gmail.com>
2017-06-06 23:55:41 -0400

    Fix share buttons

--- app/components/fb-share-button/component.js
+++ app/components/fb-share-button/component.js
@@ -1,6 +1,7 @@
 import Share from 'ember-social-share/components/fb-share-button';
 
 export default Share.extend({
+  store: Ember.inject.service(),
   shareURL: Ember.computed(function() {
     let base=  this.get('store')
                    .peekAll('option')
