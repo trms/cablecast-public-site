89356ff44c0cc740f3a5e6ab3b57ec59b39be4f6
raytiley <raytiley@gmail.com>
2018-02-07 10:13:44 -0500

    CC-761 - Show Page Layout Supports Embeded PDFS
    This brings in `ember-pdf-js` but actually uses a custom component that behaves a bit better for scaling.
    
    This alo applies a bunch of styling tweaks for the show page from Amber.
    
    TODO - drop ember-pdf-js dependency and just rely of mozilla's pdf js directly

---
+++ app/components/pdf-viewer/component.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  pdfJs: Ember.inject.service('pdf-js'),
+  didInsertElement() {
+    let pdfjs = this.get('pdfJs');
+    let url = this.get('url');
+    let [container] = this.element.getElementsByClassName('pdf-wrapper');
+    let pdfLinkService = new PDFJS.PDFLinkService();
+
+    let pdfViewer = new PDFJS.PDFViewer({
+      container: container,
+      linkService: pdfLinkService
+    });
+    window.viewer = pdfViewer;
+    pdfLinkService.setViewer(pdfViewer);
+
+    let pdfHistory = new PDFJS.PDFHistory({
+      linkService: pdfLinkService
+    });
+    pdfLinkService.setHistory(pdfHistory)
+    let pdfFindController = new PDFJS.PDFFindController({
+      pdfViewer
+    });
+    pdfViewer.setFindController(pdfFindController)
+    PDFJS.getDocument(url).then(function (pdf) {
+      pdfViewer.setDocument(pdf).then(function() {
+        pdfViewer.currentScaleValue = 'auto';
+      });
+
+      pdfLinkService.setDocument(pdf)
+      pdfHistory.initialize(pdf.fingerprint);
+    });
+    this.set('pdfViewer', pdfViewer);
+    Ember.$(window).on('resize.' + this.get('elementId'), this._handleResizeEvent.bind(this));
+  },
+
+  willDestroyElement: function() {
+        this._super();
+    Ember.$(window).off('resize.' + this.get('elementId'));
+
+  },
+
+  _handleResizeEvent() {
+    Ember.run.debounce(this, this.rescalePdf, 150);
+  },
+
+  rescalePdf() {
+    console.log('rescaling pdf...');
+    this.get('pdfViewer').currentScaleValue = 'auto';
+  }
+});
