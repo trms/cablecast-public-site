8c8e0d866e61b861fd8f0808f29b156c02984cdf
Katie Gengler <katie@kmg.io>
2021-02-17 16:27:25 -0500

    Minor cleanup

--- app/models/channel.js
+++ app/models/channel.js
@@ -1,13 +1,8 @@
-
 import DS from 'ember-data';
-// import Ember from 'ember';
-
-// var ArrayProxy = Ember.ArrayProxy.extend(Ember.PromiseProxyMixin);
 
 export default DS.Model.extend({
 	name: DS.attr('string'),
 	primaryLocation: DS.attr(),
 	publicSite: DS.belongsTo('public-site', {async: false}),
 	liveStreams: DS.hasMany('live-stream', {async: true}),
-
 });
