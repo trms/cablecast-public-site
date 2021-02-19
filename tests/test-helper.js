418311c8b5a7ca33ec4fd3bc2e8361625adcfb2e
Katie Gengler <katie@kmg.io>
2021-02-19 16:34:11 -0500

    vv3.16.2...vv3.20.2

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'cablecast-public-site/app';
+import config from 'cablecast-public-site/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
