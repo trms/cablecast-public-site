85e6a8b8566e51ee254a2b6798d5af449f19694a
Katie Gengler <katie@kmg.io>
2021-03-02 16:30:37 -0500

    Add test for document title on /shows/:id route

--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -1,23 +1,26 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
-import { setupMirage } from 'ember-cli-mirage/test-support';
+import { setupApplicationTest } from '../helpers';
 
 module('Acceptance | application', function (hooks) {
   setupApplicationTest(hooks);
-  setupMirage(hooks);
 
   test('visiting / sets page title', async function (assert) {
     let publicSite = this.server.create('public-site');
     this.server.create('channel', {
       name: 'Default Channel Name',
-      primaryLocation: 1,
+      publicSite,
+    });
+
+    this.server.create('channel', {
+      name: 'Alternate Channel Name',
       publicSite,
     });
 
     await visit('/');
 
     assert.equal(currentURL(), '/');
+
     assert.equal(document.title, 'Default Channel Name');
   });
 });
