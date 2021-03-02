6c6e39b745bcd3fc86c7cc1e6b806234c9d11263
Katie Gengler <katie@kmg.io>
2021-03-02 16:50:38 -0500

    Test for schedule route page title

---
+++ tests/acceptance/schedule-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from '../helpers';
+
+module('Acceptance | schedule', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /schedule sets page title', async function (assert) {
+    let publicSite = this.server.create('public-site');
+    let channel = this.server.create('channel', {
+      name: 'Default Channel Name',
+      publicSite,
+    });
+
+    await visit(`/schedule?channel=${channel.id}`);
+
+    assert.equal(currentURL(), `/schedule?channel=${channel.id}`);
+    assert.equal(document.title, 'Schedule');
+  });
+});
